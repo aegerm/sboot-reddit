9d18683ee7cb68f7926f41584f9e41677e023ae4
aegerm <alexandreegermarques@gmail.com>
2022-05-03 15:46:44 -0300

    Primeiro commit

---
+++ src/main/java/com/egersistemasavancados/sbootreddit/SbootRedditApplication.java
@@ -0,0 +1,12 @@
+package com.egersistemasavancados.sbootreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootRedditApplication.class, args);
+	}
+}
---
+++ src/test/java/com/egersistemasavancados/sbootreddit/SbootRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egersistemasavancados.sbootreddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
